abb9ad16ba6d676dd7db822401f26462b564bbdb
Mohd Adil <169131020+mohd-adil-2005@users.noreply.github.com>
2024-10-15 09:46:44 -0500

    Add files via upload

---
+++ adiop.c
@@ -0,0 +1,6 @@
+#include<conio.h>
+#include<math.h>
+int main(){
+    printf("this is Adil Ansar!");
+    return 0;
+}
---
+++ todo app.js
@@ -0,0 +1,7 @@
+let gameSeqs= [];
+let userSeqs= [];
+let started = false;
+let level  = 0;
+document.addEventListener("keypress",function(){
+  console.log("game is started");
+});
